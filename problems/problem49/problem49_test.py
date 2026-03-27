4fb8c0e0a2616844bd7bd66058c9acf50a40cb89
FoxtrotEuphony <sansumandrew@gmail.com>
2026-03-27 14:16:01 +0000

    test: create test from leetcode examples for problem 49

--- problems/problem49/problem49_test.py
+++ problems/problem49/problem49_test.py
@@ -3,7 +3,23 @@ import unittest
 from problem49 import Solution
 
 class Test49Solution(unittest.TestCase):
-    pass
+    def test_example1(self):
+        strs = ["eat","tea","tan","ate","nat","bat"]
+        result = Solution().groupAnagrams(strs)
+        target = [["bat"],["nat","tan"],["ate","eat","tea"]]
+        self.assertEqual(result, target)
+
+    def test_example2(self):
+        strs = [""]
+        result = Solution().groupAnagrams(strs)
+        target = [[""]]
+        self.assertEqual(result, target)
+
+    def test_example3(self):
+        strs = ["a"]
+        result = Solution().groupAnagrams(strs)
+        target = [["a"]]
+        self.assertEqual(result, target)
 
 if __name__ == "__main__":
     unittest.main()
